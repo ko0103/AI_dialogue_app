13c2b9371c4a8adeeaacef285bc6c28e0bfb2581
ko0103 <eapoestudy@gmail.com>
2025-02-22 20:48:59 +0900

    スコアのランキング表示機能、その他調整

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,4 +1,11 @@
 class ScoresController < ApplicationController
+  def index
+    @easy_scores = sort_scores("easy")
+    @normal_scores = sort_scores("normal")
+    @hard_scores = sort_scores("hard")
+    @free_scores = sort_scores("free")
+  end
+
   def show
     chat_session_id = session[:chat_session_id]
     Rails.logger.debug "ScoresController session_id: #{session.id}"
@@ -14,5 +21,11 @@ class ScoresController < ApplicationController
       flash[:error] = "チャットセッションが見つかりません"
       redirect_to homes_path and return
     end
+  end
+
+  private
+
+  def sort_scores(difficulty)
+    ChatSession.where(difficulty: difficulty).joins(:score, :user).order("scores.score DESC").limit(10).select("chat_sessions.*, scores.score AS score_display, users.name AS user_name")
   end
 end
