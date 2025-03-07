f11ec428b35dfd595f3297880bfa808090000a41
ko0103 <eapoestudy@gmail.com>
2025-03-07 20:43:50 +0900

    パスワード変更機能、ログイン時の画面遷移も改善

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  devise :database_authenticatable,
+         :registerable, :recoverable, :rememberable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   # validates :name, presence: true
   has_many :chat_sessions
   has_many :messages
