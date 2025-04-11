4457cc8fdf9e504be1a0539413d09d80f994b545
ko0103 <eapoestudy@gmail.com>
2025-04-11 22:01:58 +0900

    フッター追加、無言送信禁止追加など

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,6 @@ class Message < ApplicationRecord
   belongs_to :chat_session
   has_one :ai_response, dependent: :destroy
 
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140, message: "140字以内で入力してください!" }
+  validates :content, presence: { message: "メッセージを入力してください!" }
 end
